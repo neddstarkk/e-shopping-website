2c42327a4c2d938a362afbe35abcce5c4b7ced3d
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2019-01-12 10:36:17 +0530

    Added the models, urls, and views to display the cart and its working

--- cart/cart.py
+++ cart/cart.py
@@ -30,11 +30,9 @@ class Cart(object):
 			self.cart[product_id]['quantity'] += quantity
 		self.save()
 
-
 	def save(self):
 		# mark the session as "modified" to make sure it gets saved
 		self.session.modified = True
-
 
 	def remove(self, product):
 		"""
@@ -44,3 +42,34 @@ class Cart(object):
 		if product_id in self.cart:
 			del self.cart[product_id]
 			self.save()
+
+	def __iter__(self):
+		"""
+		Iterate over the items in the cart and get the products from the database
+		"""
+		product_ids = self.cart.keys()
+		# get the product objects and add them to the cart
+		products = Product.objects.filter(id__in=product_ids)
+
+		cart = self.cart.copy()
+		for product in products:
+			cart[str(product_ids)]['product']=product
+
+		for item in cart.values():
+			item['price'] = Decimal(item['price'])
+			item['total_price'] = item['price'] * item['quantity']
+			yield item
+
+	def __len__(self):
+		"""
+		Count all items in the cart.
+		"""
+		return sum(item['quantity'] for item in self.cart.values())
+
+	def get_total_price(self):
+		return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+	def clear(self):
+		# Remove cart from session
+		del self.session[settings.CART_SESSION_ID]
+		self.save()
